115e73c60b120023f6ec2e6a89a481e03b08e9cb
Chris Mahr <mahrtian@gmail.com>
2019-02-15 10:57:12 -0500

    update code to reflect getting through section 2, lesson 14

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log('We come in peace');
+// STRING
+var myName = 'Chris';
+// myName = 34
+// NUMBER
+var myAge = 34.3;
+// myAge = 'Chris'
+// BOOLEAN
+var lovesFood = true;
+// lovesFood = 'Of course'
+// ANY (assign types)
+var myRealAge;
+myRealAge = 34;
+myRealAge = '34';
+// ARRAY
+var hobbies = ['Cooking', 'Sports'];
+hobbies = ['100'];
+// TUPLE
+var address = ['Main Street', 99];
+// ENUM
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue"; // 2
+})(Color || (Color = {}));
+var myColor = Color.Blue;
+console.log(myColor);
--- script.ts
+++ script.ts
@@ -1,1 +1,32 @@
-console.log('We come in peace')
+// STRING
+let myName = 'Chris'
+// myName = 34
+
+// NUMBER
+let myAge = 34.3
+// myAge = 'Chris'
+
+// BOOLEAN
+let lovesFood: boolean = true
+// lovesFood = 'Of course'
+
+// ANY (assign types)
+let myRealAge
+myRealAge = 34
+myRealAge = '34'
+
+// ARRAY
+let hobbies: any[] = ['Cooking', 'Sports']
+hobbies = ['100']
+
+// TUPLE
+let address: [string, number] = ['Main Street', 99]
+
+// ENUM
+enum Color {
+    Gray, // 0
+    Green = 100, // 1
+    Blue // 2
+}
+let myColor: Color = Color.Blue
+console.log(myColor)
