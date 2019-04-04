1b9f21e38f9b62abb12b5a7f165f5224284e49c0
Chris Mahr <mahrtian@gmail.com>
2019-04-04 17:00:24 -0400

    complete Section 4, Lessons 5 and 6 (Arrow Functions)

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ function reset() {
 }
 reset();
 console.log(variable);
+// Arrow Functions
+console.log('ARROW FUNCTIONS');
+var addNumbers = function (number1, number2) {
+    return number1 + number2;
+};
+console.log(addNumbers(10, 3));
+var multiplyNumbers = function (number1, number2) { return number1 * number2; };
+console.log(multiplyNumbers(10, 3));
+var greet = function () { return console.log('Hello!'); };
+greet();
+var greetFriend = function (friend) { return console.log(friend); };
+greetFriend('Chris');
--- script.ts
+++ script.ts
@@ -17,3 +17,19 @@ function reset() {
 
 reset()
 console.log(variable)
+
+// Arrow Functions
+console.log('ARROW FUNCTIONS')
+const addNumbers = function(number1: number, number2: number): number {
+    return number1 + number2
+}
+console.log(addNumbers(10, 3))
+
+const multiplyNumbers = (number1: number, number2: number) => number1 * number2
+console.log(multiplyNumbers(10, 3))
+
+const greet = () => console.log('Hello!')
+greet()
+
+const greetFriend = (friend: string) => console.log(friend)
+greetFriend('Chris')
