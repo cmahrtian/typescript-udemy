420efbad83327c9eca32af23558e4ea5a324cf14
Chris Mahr <mahrtian@gmail.com>
2019-04-10 16:43:32 -0400

    complete Section 5, Lesson 2 (creating classes and class properties)

--- script.js
+++ script.js
@@ -1,71 +1,10 @@
 "use strict";
-// let & const
-console.log('LET & CONST');
-var variable = 'Test';
-console.log(variable);
-variable = 'Another value';
-console.log(variable);
-var maxLevels = 100;
-console.log(maxLevels);
-// maxLevels = 99 // won't work
-// Block scope
-function reset() {
-    var variable = null;
-    console.log(variable);
-}
-reset();
-console.log(variable);
-// Arrow Functions
-console.log('ARROW FUNCTIONS');
-var addNumbers = function (number1, number2) {
-    return number1 + number2;
-};
-console.log(addNumbers(10, 3));
-var multiplyNumbers = function (number1, number2) { return number1 * number2; };
-console.log(multiplyNumbers(10, 3));
-var greet = function (name) {
-    if (name === void 0) { name = 'Max'; }
-    return console.log("Hello " + name + "!");
-};
-greet('Anna');
-var greetFriend = function (friend) { return console.log(friend); };
-greetFriend('Chris');
-// Default Parameters
-console.log('DEFAULT PARAMETERS');
-var countdown = function (start) {
-    if (start === void 0) { start = 10; }
-    console.log(start);
-    while (start > 0) {
-        start--;
+var Person = /** @class */ (function () {
+    function Person(name, username) {
+        this.username = username;
+        this.name = name;
     }
-    console.log('Done!', start);
-};
-countdown();
-//Rest & Spread
-console.log('REST & SPREAD');
-var numbers = [1, 10, 99, -5];
-console.log(Math.max(1, 10, 99, -5));
-console.log(Math.max.apply(Math, numbers));
-function makeArray() {
-    var args = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        args[_i] = arguments[_i];
-    }
-    return args;
-}
-console.log(makeArray('Dan', 1));
-// Destructuring
-console.log('DESTRUCTURING');
-var myHobbies = ['Cooking', 'Sports'];
-var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
-console.log(hobby1, hobby2);
-var userData = {
-    userName: 'Chris',
-    age: 34
-};
-var userName = userData.userName, age = userData.age;
-console.log(userName, age);
-// Template Literals
-var myName = 'Chris';
-var greeting = "Hello, I'm " + myName + "!\nIt's nice to meet you!";
-console.log(greeting);
+    return Person;
+}());
+var person = new Person('Chris', 'cmahr');
+console.log(person.name, person.username);
--- script.ts
+++ script.ts
@@ -1,74 +1,12 @@
-// let & const
-console.log('LET & CONST')
-let variable = 'Test'
-console.log(variable)
-variable = 'Another value'
-console.log(variable)
+class Person {
+    name: string
+    private type: string
+    protected age: number
 
-const maxLevels = 100
-console.log(maxLevels)
-// maxLevels = 99 // won't work
-
-// Block scope
-function reset() {
-    let variable = null
-    console.log(variable)
+    constructor(name: string, public username: string) {
+        this.name = name
+    }
 }
 
-reset()
-console.log(variable)
-
-// Arrow Functions
-console.log('ARROW FUNCTIONS')
-const addNumbers = function(number1: number, number2: number): number {
-    return number1 + number2
-}
-console.log(addNumbers(10, 3))
-
-const multiplyNumbers = (number1: number, number2: number) => number1 * number2
-console.log(multiplyNumbers(10, 3))
-
-const greet = (name: string = 'Max') => console.log(`Hello ${name}!`)
-greet('Anna')
-
-const greetFriend = (friend: string) => console.log(friend)
-greetFriend('Chris')
-
-// Default Parameters
-console.log('DEFAULT PARAMETERS')
-const countdown = (start: number = 10): void => {
-    console.log(start)
-    while (start > 0) { start -- }
-    console.log('Done!', start)
-}
-countdown()
-
-//Rest & Spread
-console.log('REST & SPREAD')
-const numbers = [1, 10, 99, -5]
-console.log(Math.max(1, 10, 99, -5))
-console.log(Math.max(...numbers))
-
-function makeArray(...args: [string, number]) {
-    return args
-}
-console.log(makeArray('Dan', 1))
-
-// Destructuring
-console.log('DESTRUCTURING')
-const myHobbies = ['Cooking', 'Sports']
-const [hobby1, hobby2] = myHobbies
-console.log(hobby1, hobby2)
-
-const userData = {
-    userName: 'Chris',
-    age: 34
-}
-const {userName, age} = userData
-console.log(userName, age)
-
-// Template Literals
-const myName = 'Chris'
-const greeting = `Hello, I'm ${myName}!
-It's nice to meet you!`
-console.log(greeting)
+const person = new Person('Chris', 'cmahr')
+console.log(person.name, person.username)
