fac88f77c4d014cff92786eda08451e212ce5f7f
Chris Mahr <mahrtian@gmail.com>
2019-04-10 16:50:51 -0400

    complete Section 5, Lesson 3 (class methods and access modifiers)

--- script.js
+++ script.js
@@ -2,9 +2,20 @@
 var Person = /** @class */ (function () {
     function Person(name, username) {
         this.username = username;
+        this.age = 34;
         this.name = name;
     }
+    Person.prototype.printAge = function () {
+        console.log(this.age);
+        this.setType('Bon vivant');
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+        console.log(this.type);
+    };
     return Person;
 }());
 var person = new Person('Chris', 'cmahr');
 console.log(person.name, person.username);
+person.printAge();
+// person.setType('Web developer')
--- script.ts
+++ script.ts
@@ -1,12 +1,24 @@
 class Person {
     name: string
     private type: string
-    protected age: number
+    protected age: number = 34
 
     constructor(name: string, public username: string) {
         this.name = name
+    }
+
+    printAge() {
+        console.log(this.age)
+        this.setType('Bon vivant')
+    }
+
+    private setType(type: string) {
+        this.type = type
+        console.log(this.type)
     }
 }
 
 const person = new Person('Chris', 'cmahr')
 console.log(person.name, person.username)
+person.printAge()
+// person.setType('Web developer')
