46a4b226bfb7f84db9beaee7881daf5c774e6bb3
Chris Mahr <mahrtian@gmail.com>
2019-05-01 15:20:36 -0400

    complete Section 7, Lesson 4 ('Interfaces And Methods')

--- script.js
+++ script.js
@@ -7,8 +7,13 @@ function changeName(person) {
 }
 var person = {
     firstName: 'Chris',
-    hobbies: ['cooking', 'sports', 'coding']
+    hobbies: ['cooking', 'sports', 'coding'],
+    greet: function (lastName) {
+        console.log("Hi, I am " + this.firstName + " " + lastName);
+    }
 };
-greet({ firstName: 'Chris', age: 34 });
+// greet({firstName: 'Chris', age: 34})
+greet(person);
 changeName(person);
 greet(person);
+person.greet('Mahr');
--- script.ts
+++ script.ts
@@ -2,6 +2,7 @@ interface NamedPerson {
     firstName: string
     age?: number
     [propName: string]: any
+    greet(lastName: string): void
 }
 
 function greet(person: NamedPerson) {
@@ -12,11 +13,16 @@ function changeName(person: NamedPerson) {
     person.firstName = 'Dan'
 }
 
-const person = {
+const person: NamedPerson = {
     firstName: 'Chris',
-    hobbies: ['cooking', 'sports', 'coding']
+    hobbies: ['cooking', 'sports', 'coding'],
+    greet(lastName: string) {
+        console.log(`Hi, I am ${this.firstName} ${lastName}`)
+    }
 }
 
-greet({firstName: 'Chris', age: 34})
+// greet({firstName: 'Chris', age: 34})
+greet(person)
 changeName(person)
 greet(person)
+person.greet('Mahr')
