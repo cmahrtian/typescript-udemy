f3669754a687d156ee6bb921fef75003689e56c4
Chris Mahr <mahrtian@gmail.com>
2019-05-07 17:07:17 -0400

    complete Section 8, Lesson 3 ('Creating a Generic Function')

--- script.js
+++ script.js
@@ -3,6 +3,13 @@
 function echo(data) {
     return data;
 }
-console.log(echo('Chris').length);
+console.log(echo('Chris'));
 console.log(echo(27));
 console.log(echo({ name: 'Chris', age: 34 }).length);
+// Better Generic
+function betterEcho(data) {
+    return data;
+}
+console.log(betterEcho('Chris').length);
+console.log(betterEcho(27));
+console.log(echo({ name: 'Chris', age: 34 }));
--- script.ts
+++ script.ts
@@ -3,6 +3,15 @@ function echo(data: any) {
     return data
 }
 
-console.log(echo('Chris').length)
+console.log(echo('Chris'))
 console.log(echo(27))
 console.log(echo({name: 'Chris', age: 34}).length)
+
+// Better Generic
+function betterEcho<T>(data: T) {
+    return data
+}
+
+console.log(betterEcho('Chris').length)
+console.log(betterEcho<number>(27))
+console.log(echo({name: 'Chris', age: 34}))
