eacf3d6e31018a5b17ec9ce173da45e6b7ef1c38
Chris Mahr <mahrtian@gmail.com>
2019-05-07 17:03:16 -0400

    complete Section 8, Lesson 2 ('Generics: Why and What')

--- script.js
+++ script.js
@@ -1,45 +1,8 @@
 "use strict";
-function greet(person) {
-    console.log("Hello, " + person.firstName);
+// Simple Generic
+function echo(data) {
+    return data;
 }
-function changeName(person) {
-    person.firstName = 'Dan';
-}
-var person = {
-    firstName: 'Chris',
-    hobbies: ['cooking', 'sports', 'coding'],
-    greet: function (lastName) {
-        console.log("Hi, I am " + this.firstName + " " + lastName);
-    }
-};
-// greet({firstName: 'Chris', age: 34})
-greet(person);
-changeName(person);
-greet(person);
-person.greet('Mahr');
-var Person = /** @class */ (function () {
-    function Person() {
-    }
-    Person.prototype.greet = function (lastName) {
-        console.log("Hi, I am " + this.firstName + " " + lastName);
-    };
-    return Person;
-}());
-var myPerson = new Person();
-myPerson.firstName = 'Chris';
-myPerson.lastName = 'Null';
-greet(myPerson);
-myPerson.greet(myPerson.lastName);
-var myDoubleFunction;
-myDoubleFunction = function (value1, value2) {
-    return (value1 + value2) * 2;
-};
-console.log(myDoubleFunction(10, 20));
-var oldPerson = {
-    age: 34,
-    firstName: 'Chris',
-    greet: function (lastName) {
-        console.log('Hello!');
-    }
-};
-console.log(oldPerson);
+console.log(echo('Chris').length);
+console.log(echo(27));
+console.log(echo({ name: 'Chris', age: 34 }).length);
--- script.ts
+++ script.ts
@@ -1,70 +1,8 @@
-interface NamedPerson {
-    firstName: string
-    age?: number
-    [propName: string]: any
-    greet(lastName: string): void
+// Simple Generic
+function echo(data: any) {
+    return data
 }
 
-function greet(person: NamedPerson) {
-    console.log(`Hello, ${person.firstName}`)
-}
-
-function changeName(person: NamedPerson) {
-    person.firstName = 'Dan'
-}
-
-const person: NamedPerson = {
-    firstName: 'Chris',
-    hobbies: ['cooking', 'sports', 'coding'],
-    greet(lastName: string) {
-        console.log(`Hi, I am ${this.firstName} ${lastName}`)
-    }
-}
-
-// greet({firstName: 'Chris', age: 34})
-greet(person)
-changeName(person)
-greet(person)
-person.greet('Mahr')
-
-class Person implements NamedPerson {
-    firstName!: string
-    lastName!: string
-    greet(lastName: string) {
-        console.log(`Hi, I am ${this.firstName} ${lastName}`)
-    }
-}
-
-const myPerson = new Person()
-myPerson.firstName = 'Chris'
-myPerson.lastName = 'Null'
-greet(myPerson)
-myPerson.greet(myPerson.lastName)
-
-// Function Types
-interface DoubleValueFunc {
-    (number1: number, number2: number): number
-}
-
-let myDoubleFunction: DoubleValueFunc
-myDoubleFunction = function(value1: number, value2: number) {
-    return (value1 + value2) * 2
-}
-
-console.log(myDoubleFunction(10, 20))
-
-// Interface Inheritance
-
-interface AgedPerson extends NamedPerson {
-    age: number
-}
-
-const oldPerson: AgedPerson = {
-    age: 34,
-    firstName: 'Chris',
-    greet(lastName: string) {
-        console.log('Hello!')
-    }
-}
-
-console.log(oldPerson)
+console.log(echo('Chris').length)
+console.log(echo(27))
+console.log(echo({name: 'Chris', age: 34}).length)
