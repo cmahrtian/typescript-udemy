9254212b43d3e2effb1480690c1093516fb6e0a8
Chris Mahr <mahrtian@gmail.com>
2019-05-07 16:36:15 -0400

    complete Section 7, Lesson 7 ('Interface Inheritance')

--- script.js
+++ script.js
@@ -35,3 +35,11 @@ myDoubleFunction = function (value1, value2) {
     return (value1 + value2) * 2;
 };
 console.log(myDoubleFunction(10, 20));
+var oldPerson = {
+    age: 34,
+    firstName: 'Chris',
+    greet: function (lastName) {
+        console.log('Hello!');
+    }
+};
+console.log(oldPerson);
