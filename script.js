48e83904f95d83e31112e1b0f21d5c8c0915875e
Chris Mahr <mahrtian@gmail.com>
2019-05-07 17:41:34 -0400

    complete Section 8 on Generics, including problem set

--- script.js
+++ script.js
@@ -38,3 +38,30 @@ var simpleMath = new SimpleMath();
 simpleMath.baseValue = '10';
 simpleMath.mutiplyValue = 20;
 console.log(simpleMath.calculate());
+var MyMap = /** @class */ (function () {
+    function MyMap() {
+        this.map = {};
+    }
+    MyMap.prototype.setItem = function (key, item) {
+        this.map[key] = item;
+    };
+    MyMap.prototype.getItem = function (key) {
+        return this.map[key];
+    };
+    MyMap.prototype.clear = function () {
+        this.map = {};
+    };
+    MyMap.prototype.printMap = function () {
+        for (var key in this.map) {
+            console.log(key, this.map[key]);
+        }
+    };
+    return MyMap;
+}());
+var numberMap = new MyMap();
+numberMap.setItem('apples', 10);
+numberMap.setItem('bananas', 2);
+console.log(numberMap.getItem('apples'));
+numberMap.printMap();
+numberMap.clear();
+numberMap.printMap();
--- script.ts
+++ script.ts
@@ -45,3 +45,33 @@ const simpleMath = new SimpleMath<string, number>()
 simpleMath.baseValue = '10'
 simpleMath.mutiplyValue = 20
 console.log(simpleMath.calculate())
+
+class MyMap<T> {
+    private map: {[key: string]: T} = {}
+
+    setItem(key: string, item: T) {
+        this.map[key] = item
+    }
+
+    getItem(key: string) {
+        return this.map[key]
+    }
+
+    clear() {
+        this.map = {}
+    }
+
+    printMap() {
+        for (let key in this.map) {
+            console.log(key, this.map[key])
+        }
+    }
+}
+
+const numberMap = new MyMap<number>()
+numberMap.setItem('apples', 10)
+numberMap.setItem('bananas', 2)
+console.log(numberMap.getItem('apples'))
+numberMap.printMap()
+numberMap.clear()
+numberMap.printMap()
